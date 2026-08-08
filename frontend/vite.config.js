9aa74d9275a220f5c18a28b960693c06b6cab1cc
Diyajain3 <diyajain3003@gmail.com>
2026-08-09 01:56:41 +0530

    enhancing ui

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+  },
   preview: {
+    host: '0.0.0.0',
     allowedHosts: true,
   },
 })
