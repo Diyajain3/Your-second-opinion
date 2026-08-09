6a80f5ec9181bc0fe0902bb3a24853b6b0ddb3bc
Diyajain3 <diyajain3003@gmail.com>
2026-08-10 01:58:07 +0530

    remove hardcoded credentials

--- backend/src/db/prisma.js
+++ backend/src/db/prisma.js
@@ -1,11 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-const dbUrl =
-  process.env.DATABASE_URL ||
-  "postgresql://postgres.rqlbmopctmvukmfafotl:diya3030jain@aws-0-ap-northeast-1.pooler.supabase.com:5432/postgres";
-
-const prisma = new PrismaClient({
-  datasourceUrl: dbUrl,
-});
+const prisma = new PrismaClient();
 
 export default prisma;
