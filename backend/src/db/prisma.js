533fd3320f90a19bc4707f6ac8f85fdbce12e022
Diyajain3 <diyajain3003@gmail.com>
2026-08-10 01:51:23 +0530

    fix supabase pooler hostname

--- backend/src/db/prisma.js
+++ backend/src/db/prisma.js
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const dbUrl =
   process.env.DATABASE_URL ||
-  "postgresql://postgres:diya3030jain@db.rqlbmopctmvukmfafotl.supabase.co:6543/postgres?pgbouncer=true";
+  "postgresql://postgres.rqlbmopctmvukmfafotl:diya3030jain@aws-0-ap-northeast-1.pooler.supabase.com:6543/postgres?pgbouncer=true";
 
 const prisma = new PrismaClient({
   datasourceUrl: dbUrl,
