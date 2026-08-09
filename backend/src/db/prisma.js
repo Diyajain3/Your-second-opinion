8c5f799c2405e4ac581de02feb30174bb8916a69
Diyajain3 <diyajain3003@gmail.com>
2026-08-10 01:43:30 +0530

    fix prisma client runtime datasourceUrl

--- backend/src/db/prisma.js
+++ backend/src/db/prisma.js
@@ -1,5 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const dbUrl =
+  process.env.DATABASE_URL ||
+  "postgresql://postgres:diya3030jain@db.rqlbmopctmvukmfafotl.supabase.co:6543/postgres?pgbouncer=true";
+
+const prisma = new PrismaClient({
+  datasourceUrl: dbUrl,
+});
 
 export default prisma;
